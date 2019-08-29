8fd381552ea38dabca821745ced6915611aa6e19
qttq23 <50412685+qttq23@users.noreply.github.com>
2019-08-30 01:32:44 +0700

    first commit

---
+++ Model/StudentModel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryMVVM.Model
+{
+    class StudentModel
+    {
+        
+    }
+
+    class Student : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void RaisePropertyChanged(string propertyName)
+        {
+            if(PropertyChanged != null)
+            {
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        public string FirstName {
+            get =>firstName;
+            set {
+                if(value != firstName)
+                {
+                    firstName = value;
+                    RaisePropertyChanged("FirstName");
+                    RaisePropertyChanged("FullName");
+                }
+            }
+        }
+        public string LastName
+        {
+            get => lastName;
+            set
+            {
+                if (value != lastName)
+                {
+                    lastName = value;
+                    RaisePropertyChanged("LastName");
+                    RaisePropertyChanged("FullName");
+                }
+            }
+        }
+
+        public string FullName
+        {
+            get => FirstName + LastName;
+          
+        }
+        private string firstName;
+        private string lastName;
+
+    }
+}
---
+++ ViewModel/StudentViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using TryMVVM.Model;
+
+namespace TryMVVM.ViewModel
+{
+    class StudentViewModel
+    {
+        public ObservableCollection<Student> Students { get; set; }
+
+        public void LoadStudents()
+        {
+            var someNewStudents = new ObservableCollection<Student>()
+            {
+                new Student(){FirstName="Bui", LastName="Thang"},
+                new Student(){FirstName="Irous", LastName="Quang"},
+                new Student(){FirstName="Mr", LastName="Irous"},
+            };
+
+            Students = someNewStudents;
+        }
+
+    }
+}
